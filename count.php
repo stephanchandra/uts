8d9dc3c7d7d876e5b6964766c15fbf2b362e29fa
stephanchandra <38389842+stephanchandra@users.noreply.github.com>
2023-04-27 12:58:20 +0700

    Update count.php

--- count.php
+++ count.php
@@ -6,7 +6,7 @@
 
     $connect = mysqli_connect($host, $username, $password, $db);
 
-    if ($connect -> connect_errno){
+    if ($connect -> connect_errno) {
         echo "Connection Failed: " . $connect -> connect_error;
         exit();
     }
